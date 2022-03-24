0a106b8243fb88fb85a6aab8587bb955ac03e555
Cameron Rosier <c.rosier00@gmail.com>
2022-03-24 18:46:49 +0000

    Test code

--- vars/readVersionFromFile.groovy
+++ vars/readVersionFromFile.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 import org.apache.commons.lang3.StringUtils
-import groovy.io.FileType.FILES
+import groovy.io.FileType
 
 def call(String workspace, String versionFileName = "VERSION") {
     File versionFile = new File(workspace, versionFileName)
