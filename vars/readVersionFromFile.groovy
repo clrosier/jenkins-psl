427d3d1557213d20f8697166e5fa356a60ee4d0d
Cameron Rosier <c.rosier00@gmail.com>
2022-03-24 18:45:08 +0000

    Test code

--- vars/readVersionFromFile.groovy
+++ vars/readVersionFromFile.groovy
@@ -1,10 +1,16 @@
 #!/usr/bin/env groovy
 import org.apache.commons.lang3.StringUtils
+import static.groovy.io.FileType.FILES
 
 def call(String workspace, String versionFileName = "VERSION") {
     File versionFile = new File(workspace, versionFileName)
     String version = versionFile.text
-    
+
+    def dir = new File(workspace)
+    def files = [];
+    dir.traverse(type: FILES, maxDepth: 0) { files.add(it) };
+    println(files)
+
     def parser = /(?<major>\d+).(?<minor>\d+).(?<patch>\d+)/
     def match = version =~ parser
     if (match.matches()) {
